e63b97eb901581d48b659e1ee3f6e61731ee3e65
Hossam Doma <ConanCode96@gmail.com>
2016-05-18 03:03:30 +0200

    Update and rename LICENSE.md to Weighed Union-Find with Path Compression.java

---
+++ Weighed Union-Find with Path Compression.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class main {
+
+	static int[] id = new int[10];
+	static int[] sz = new int[10];
+
+	public static void main(String[] args) {
+
+		Arrays.fill(sz, 1);
+		
+		//get the input
+		
+		//process your queries
+		
+		//get the output done! :)
+		
+	}
+
+	private boolean find(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	private int root(int i) {
+
+		while (i != id[i]) {
+
+			// Simple path-compression (link child to grandparent).
+			id[i] = id[id[i]]; // one line to keep it ALMOST flat.
+
+			i = id[i];
+		}
+
+		return i;
+
+	}
+
+	private void union(int p, int q) {
+
+		int i = root(p);
+		int j = root(q);
+
+		// Keeping it logarithmic O(log N) at most.
+		// Linking the smaller tree to the bigger.
+
+		if (sz[i] >= sz[j]) {
+			id[j] = i;
+			sz[i] += sz[j];
+		} else {
+			id[i] = j;
+			sz[j] += sz[i];
+		}
+	}
+
+}
