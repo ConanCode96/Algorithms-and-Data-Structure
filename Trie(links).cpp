c77a260d359e93d6eebc7c6e4e18a3ef697be4e2
Hossam Doma <ConanCode96@gmail.com>
2018-02-16 16:04:35 +0200

    Create Trie(links).cpp

---
+++ Trie(links).cpp
@@ -0,0 +1,101 @@
+/*
+    Submitted With (^^) by -> Hossam ^_^
+     ___     ___
+    (^_^)   (^_^)
+   /( | )\ /( | )\
+     | |     | |
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline void prep(){
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	#ifdef Cyborg101
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+#define X first
+#define Y second
+#define ll long long
+#define inf (0x7f7f7f7f7f7f7f7f)
+#define all(x) x.begin(), x.end()
+
+const int MX = 1e6 + 10;
+const int mood = ((int)1e9 + 7);
+
+/////////////////////////////////////
+
+struct node{
+	node* lf;
+	node* rg;
+	int cnt = 0;
+};
+
+void insert(int x, node* cur){
+
+	for(int i = 30; i >= 0; --i){
+		int val = (1 << i) & x;
+		if(val == 0){
+			if(cur->lf == 0)
+				cur->lf = new node();
+			cur = cur->lf;
+			cur->cnt++;
+		}else{
+			if(cur->rg == 0)
+				cur->rg = new node();
+			cur = cur->rg;
+			cur->cnt++;
+		}
+	}
+
+}
+
+int query(int x, node* cur){
+
+	for(int i = 30; i >= 0; --i){
+		int val = (1 << i) & x;
+		if(val == 0){
+			if(cur -> lf == 0)
+				return 0;
+			cur = cur -> lf;
+		}
+		else{
+			if(cur -> rg == 0)
+				return 0;
+			cur = cur -> rg;
+		}
+	}
+
+	return cur -> cnt;
+}
+
+int main(){
+
+	prep();
+	
+	int n;
+	cin >> n;
+
+	node* root = new node();
+
+	for(int i = 0; i < n; ++i){
+		int x;
+		cin >> x;
+		insert(x, root);
+	}
+
+	int q;
+	cin >> q;
+	for(int i = 0; i < q; ++i){
+		int x;
+		cin >> x;
+		cout << query(x, root) << endl;
+	}
+	
+    return 0;
+}	
