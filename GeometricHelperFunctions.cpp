3fba3f8643a8aee50a42e1c4576bf0ca1272536b
Hossam <conancode96@gmail.com>
2018-09-05 12:05:44 +0200

    added point struct and helper functions inside

--- GeometricHelperFunctions.cpp
+++ GeometricHelperFunctions.cpp
@@ -11,3 +11,23 @@
     auto manhattan = [](pair<int, int> p1, pair<int, int> p2){
         return abs(p1.X - p2.X) + abs(p1.Y - p2.Y);
     };
+
+    struct point
+    {
+        double x, y;
+
+        double operator^(point &b) const
+        {
+            return x * b.y - y * b.x;
+        };
+
+        double operator*(point &b) const
+        {
+            return x * b.x + y * b.y;
+        };
+
+        double mag()
+        {
+            return hypot(x, y);
+        };
+    };
